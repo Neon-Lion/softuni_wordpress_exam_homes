3d9f7dd59bd9a1fff6c05905baa89998f054db81
Neon-Lion <ivoadjemov@gmail.com>
2023-04-30 10:07:29 +0300

    Create footer.php file for the footer part of the theme

---
+++ wp-content/themes/softuni-homes/footer.php
@@ -0,0 +1,12 @@
+            <footer class="site-footer">
+                <p>&copy; Copyright <?php echo date( 'Y' ); ?> | Developer links: 
+                    <a href="/edits.html">Edits</a>,
+                    <a href="/index.html">Home</a>,
+                    <a href="/single.html">Single</a>
+                </p>
+            </footer>
+        </div>
+    
+        <?php wp_footer(); ?>
+    </body>
+</html>
