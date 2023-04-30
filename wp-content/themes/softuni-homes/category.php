f9b70a99e0afb093747b163a08adae2e684fab7b
Neon-Lion <ivoadjemov@gmail.com>
2023-04-30 13:32:42 +0300

    Create dedicated archive template for the category archive of blog posts

---
+++ wp-content/themes/softuni-homes/category.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+    <h1><?php the_archive_title(); ?></h1>
+    <?php if( category_description() ) : ?>
+        <div><?php _e( category_description(), 'softuni') ?></div>
+    <?php endif ?>
+	<ul class="properties-listing">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : ?>
+				<?php the_post(); ?>
+				<?php get_template_part( 'partials/content', 'category' ); ?>
+			<?php endwhile ?>
+			<?php posts_nav_link(); ?>
+		<?php else : ?>		
+				<?php _e( 'Not found posts.', 'softuni' ); ?>
+		<?php endif ?>
+	</ul>
+<?php get_footer(); ?>
