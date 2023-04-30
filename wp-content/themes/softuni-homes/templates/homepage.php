c4ac52b11010a03a7f6610e903da5418f6713b07
Neon-Lion <ivoadjemov@gmail.com>
2023-04-30 16:50:06 +0300

    Create templates/ folder for custom page templates

---
+++ wp-content/themes/softuni-homes/templates/homepage.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Template Name: Homepage
+ */
+
+// Custom Homepage
+get_header();
+?>
+	<ul class="properties-listing">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : ?>
+				<?php the_post(); ?>
+				<?php get_template_part( 'partials/content', 'home' ); ?>
+			<?php endwhile ?>
+			<?php posts_nav_link(); ?>
+		<?php else : ?>		
+				<?php _e( 'Not found posts.', 'softuni' ); ?>
+		<?php endif ?>
+	</ul>
+<?php get_footer(); ?>
