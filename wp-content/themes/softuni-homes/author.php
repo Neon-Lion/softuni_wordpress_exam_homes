40380dba12dea417f8aae143999bbe8f47b1b8aa
Neon-Lion <ivoadjemov@gmail.com>
2023-04-30 13:33:09 +0300

    Create dedicated archive template for the author archive of blog posts

---
+++ wp-content/themes/softuni-homes/author.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+    <h1><?php the_archive_title(); ?></h1>
+    <div class="author-bio"><?php echo the_author_meta( 'user_description' ); ?></div>
+	<ul class="properties-listing">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : ?>
+				<?php the_post(); ?>
+				<?php get_template_part( 'partials/content', 'author' ); ?>
+			<?php endwhile ?>
+			<?php posts_nav_link(); ?>
+		<?php else : ?>		
+				<?php _e( 'Not found posts.', 'softuni' ); ?>
+		<?php endif ?>
+	</ul>
+<?php get_footer(); ?>
