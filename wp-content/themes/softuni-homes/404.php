47e347a1f3227c314c8774fee92f273f9ae9b5e8
Neon-Lion <ivoadjemov@gmail.com>
2023-05-02 22:47:34 +0300

    Add page for error 404

---
+++ wp-content/themes/softuni-homes/404.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+    <h2>404 - Page Not Found</h2>
+    <div><?php _e( 'Sorry, nothing found', 'softuni' ); ?></div>
+    <?php echo get_search_form(); ?>
+<?php get_footer(); ?>
